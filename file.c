be533bfc432497aa47be3a392d8e37e6c32fc6f7
loheagn <loheagn@icloud.com>
2024-04-13 15:13:55 +0800

    enable chunk read
    Signed-off-by: loheagn <loheagn@icloud.com>

--- file.c
+++ file.c
@@ -5,9 +5,211 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mpage.h>
+#include <linux/mm.h>
+#include <linux/pagemap.h>
+#include <linux/path.h>
+#include <linux/namei.h>
 
 #include "bitmap.h"
 #include "simplefs.h"
+
+// const char *src = "hello world";
+
+#define RRW_KEY_LENGTH 32
+
+static struct buffer_head *read_by_iblock(struct inode *inode, sector_t iblock)
+{
+    struct buffer_head *bh = NULL;
+    struct super_block *sb = inode->i_sb;
+    struct simplefs_sb_info *sbi = SIMPLEFS_SB(sb);
+    struct simplefs_inode_info *ci = SIMPLEFS_INODE(inode);
+    struct simplefs_file_ei_block *index;
+    struct buffer_head *bh_index;
+    int ret = 0, bno;
+    uint32_t extent;
+
+    /* If block number exceeds filesize, fail */
+    if (iblock >= SIMPLEFS_MAX_BLOCKS_PER_EXTENT * SIMPLEFS_MAX_EXTENTS) {
+        return NULL;
+    }
+
+    /* Read directory block from disk */
+    bh_index = sb_bread(sb, ci->ei_block);
+    if (!bh_index) {
+        return NULL;
+    }
+    index = (struct simplefs_file_ei_block *) bh_index->b_data;
+
+    extent = simplefs_ext_search(index, iblock);
+    if (extent == -1) {
+        goto brelse_index;
+    }
+    bno = index->extents[extent].ee_start + iblock -
+          index->extents[extent].ee_block;
+
+    bh = sb_bread(sb, bno);
+
+brelse_index:
+    brelse(bh_index);
+
+    return bh;
+}
+
+const char hex_table[16] = "0123456789abcdef";
+
+static void hex_encode(char *dst, const char *src)
+{
+    int j = 0;
+    for (int i = 0; i < 32; i++) {
+        char v = *(src + i);
+        dst[j++] = hex_table[v >> 4];
+        dst[j++] = hex_table[v & 0xf];
+    }
+    dst[j] = 0x0;
+}
+
+const char *NFS_PATH_PREFIX = "/root/tarball/nfs_blocks/";
+const char *LOCAL_PATH_PREFIX = "/root/local_blocks/";
+
+static char *local_path(const char *key)
+{
+    int len = strlen(key) + strlen(LOCAL_PATH_PREFIX) + 1;
+    char *result = kmalloc(len, GFP_KERNEL);
+
+    snprintf(result, len, "%s%s", LOCAL_PATH_PREFIX, key);
+    return result;
+}
+
+static char *nfs_path(const char *key)
+{
+    int len = strlen(key) + strlen(NFS_PATH_PREFIX) + 1;
+    char *result = kmalloc(len, GFP_KERNEL);
+
+    snprintf(result, len, "%s%s", NFS_PATH_PREFIX, key);
+    return result;
+}
+
+static void my_custom_readahead(struct readahead_control *rac)
+{
+    struct inode *inode = rac->file->f_inode;
+    // static const char *src = "Hello, this is sample data from kernel space!";
+    // static const loff_t data_size = 44;  // 长度包括末尾的'\0'
+    // pr_info("loheagn get into");
+    // loff_t data_size = 11;
+    // loff_t pos = readahead_pos(rac);                // 文件中的起始位置
+    unsigned long index = readahead_index(rac);     // 起始页索引
+    unsigned long nr_pages = readahead_count(rac);  // 预读页数
+
+    unsigned long chunk_pre_block = PAGE_SIZE / RRW_KEY_LENGTH;
+
+    unsigned long iblock = index / chunk_pre_block;
+
+    struct buffer_head *bh = NULL;
+    char *keys = NULL;
+
+    bh = read_by_iblock(inode, iblock);
+    if (!bh) {
+        // pr_info("loheagn get into bh null");
+        return;
+    }
+    keys = (char *) bh->b_data;
+
+    char *file_path = NULL;
+
+    // 遍历所有需要读取的页
+    for (unsigned long i = 0; i < nr_pages; i++, index++) {
+        // pr_info("loheagn get into for i %d", i);
+        struct page *page = readahead_page(rac);  // 获取一个新的页
+        char *page_data;
+        if (!page)
+            continue;
+
+        // 获取页内部的数据地址
+        page_data = kmap(page);
+
+        // pr_info("loheagn got page data");
+
+        unsigned long chunk_idx = index + i;  // 要读第几个chunk
+
+        if (chunk_idx >= iblock * chunk_pre_block) {
+            iblock++;
+            if (bh) {
+                brelse(bh);
+            }
+
+            bh = read_by_iblock(inode, iblock);
+            keys = (char *) bh->b_data;
+        }
+
+        char key[65];
+        hex_encode(key, keys + (chunk_idx % chunk_pre_block) * RRW_KEY_LENGTH);
+
+        file_path = local_path(key);
+
+        struct path path;
+        if (kern_path(file_path, 0, &path)) {
+            // file not exist
+            kfree(file_path);
+            file_path = nfs_path(key);
+        } else {
+            // file exists, release path
+            path_put(&path);
+        }
+
+        struct file *f = filp_open(file_path, O_RDONLY, 0);
+        if (IS_ERR_OR_NULL(f)) {
+            printk(KERN_ERR "loheagn3 Failed to open file %s\n", file_path);
+            goto end;
+        }
+
+        size_t length = PAGE_SIZE;
+        if (length > f->f_inode->i_size) {
+            length = f->f_inode->i_size;
+        }
+        // pr_info("loheagn length %d", length);
+
+        loff_t offset = 0;
+        kernel_read(f, page_data, length, &offset);
+
+        filp_close(f, NULL);
+
+        if (file_path) {
+            kfree(file_path);
+            file_path = NULL;
+        }
+
+
+
+        // 清零剩余部分
+        if (length < PAGE_SIZE) {
+            memset(page_data + length, 0, PAGE_SIZE - length);
+        }
+
+        // pr_info("loheagn copy done length %d", length);
+
+        // 解除页的映射
+        kunmap(page);
+
+        // 将页标记为已更新并解锁
+        SetPageUptodate(page);
+        unlock_page(page);
+
+        // 将页放入页缓存
+        put_page(page);
+
+        // pr_info("loheagn end for length %d", length);
+    }
+
+end:
+
+    if (file_path) {
+        kfree(file_path);
+    }
+
+    if (bh) {
+        brelse(bh);
+    }
+}
 
 /* Associate the provided 'buffer_head' parameter with the iblock-th block of
  * the file denoted by inode. Should the specified block be unallocated and the
@@ -235,7 +437,8 @@ end:
 
 const struct address_space_operations simplefs_aops = {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0)
-    .readahead = simplefs_readahead,
+    // .readahead = simplefs_readahead,
+    .readahead = my_custom_readahead,
 #else
     .readpage = simplefs_readpage,
 #endif
